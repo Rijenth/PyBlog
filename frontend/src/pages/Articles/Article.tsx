d210dffd5414303cccbdd38f9cefe1924b192ee1
A_Rijenth <rijenth93100@laposte.net>
2022-12-17 10:33:22 +0100

    preparing Delete and Post route

--- frontend/src/pages/Articles/Article.tsx
+++ frontend/src/pages/Articles/Article.tsx
@@ -1,7 +1,11 @@
 import GetArticle from './getArticle';
 import IndexArticles from './IndexArticles';
+import DeleteArticle from './DeleteArticle';
+import PostArticle from './PostArticle';
 
 export { 
-    GetArticle, 
-    IndexArticles 
+    IndexArticles,
+    GetArticle,
+    DeleteArticle,
+    PostArticle
 }
