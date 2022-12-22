afcd853438dcb9afd675354e0eaaec8b5abc6dbe
A_Rijenth <rijenth93100@laposte.net>
2022-12-22 20:19:02 +0100

    fixing component not handling url manual change

--- frontend/src/pages/Articles/Get.tsx
+++ frontend/src/pages/Articles/Get.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
-import NotFound from '../404';
 
 interface Article {
     id: number;
@@ -11,43 +10,47 @@ interface Article {
     date: string;
 }
 
-
 const GetArticle = () => {
     const [isLoading, setIsLoading] = React.useState(true);
-
     const baseUrl = 'http://localhost:5000/api/articles';
     const { id } = useParams();    
     const [article, setArticle] = React.useState<Article[]>([]);
+    const [error, setError] = React.useState(null);
 
     React.useEffect(() => {
-        axios.get(`${baseUrl}/${id}`)
-            .then((response) => {
-                setArticle(response.data)
+        const getArticle = async () => {
+            try {
+                const response = await axios.get(`${baseUrl}/${id}`);
+                setArticle(response.data);
                 setIsLoading(false);
-            })
-            .catch((error) => console.log(error));
-    }, []);
+            } catch (err) {
+                setError(error);
+                setArticle([]);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getArticle();
+    }, [isLoading]);
 
     if (isLoading) {
         return <div/>;
     }
-    
-    if(article.length === 0) {
-        return <NotFound />
-    }
 
     return (
-        <div className="container">
-            {article && article.map((article) => (
-                <div key={article.id} className="card" style={{width: 500}}>
-                    <div className="card-body">
-                        <h5 className="card-title">{article.title}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{article.author} - {article.date}</h6>
-                        <p className="card-text">{article.body}</p>
+        (article.length === 0) ? <Navigate to="/404" /> : (
+            <div className="container">
+                {article && article.map((article) => (
+                    <div key={article.id} className="card" style={{width: 500}}>
+                        <div className="card-body">
+                            <h5 className="card-title">{article.title}</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">{article.author} - {article.date}</h6>
+                            <p className="card-text">{article.body}</p>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        )
     );
 };
 
